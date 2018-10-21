811cb9f4fb4067434e45c4af5946186add5a754d
Erico Silva <augusto.ericosilva@gmail.com>
2018-10-20 21:07:44 -0400

    Remove pipenv files; rename default handlers

---
+++ tests/unit/test_middleware.py
@@ -0,0 +1,27 @@
+from src.middleware import aws_api_gateway
+
+
+def assert_common_conditions(response: dict) -> None:
+    assert isinstance(response, dict)
+
+    assert response.get('statusCode') and isinstance(response['statusCode'], int)
+
+    if response.get('body'):
+        assert isinstance(response['body'], str)
+
+    assert not any(key not in ['statusCode', 'body'] for key in response)
+
+
+def test_should_1():
+    # given
+    @aws_api_gateway()
+    def any_lambda_handler(_, __):
+        return {
+            "statusCode": 200
+        }
+
+    # when
+    response = any_lambda_handler(None, None)
+
+    # then
+    assert_common_conditions(response)
